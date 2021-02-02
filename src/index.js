aa18b157e9c7ef507d940f8bc1340b6c6f061d86
rascasse-monaco <mktwkhr@gmail.com>
2021-02-03 03:05:08 +0900

    React Hooks入門

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, {　useState, useEffect　} from "react";
 import ReactDOM from 'react-dom';
 
-const Example = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    const effect = document.getElementById('effect');
-    effect.innerHTML = `useEffectが描画:You Clicked ${count} times.`;
-    document.title = `You clicked ${count + 100} times`;
-  });
+const App = () => {
+  const [count, setCount] = useState(10);
 
-  return (
+  const dec = () => {
+    setCount(count - 1);
+  };
+
+  const inc = () => {
+    setCount(count + 1);
+  };
+
+  return(
     <div>
-      <p>関数のreturnのJSXに記述して描画:You Clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      <p>通常描画:You Clicked {count} times.</p>
+      <p>Count: {count}</p>
+      <button onClick={dec}>-</button>
+      <button onClick={inc}>+</button>
     </div>
   );
-}
+};
 
-ReactDOM.render (
-  <Example />,
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
+
+
